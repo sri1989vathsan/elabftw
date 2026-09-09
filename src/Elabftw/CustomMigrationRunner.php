54b89563112a2b291903f35b9b2d6549d66d4bab
Srivathsan Adivarahan <srivathsan.adivarahan@bsse.ethz.ch>
2026-09-09 10:35:05 +0200

    usability: register migration 051 in CustomMigrationRunner

--- src/Elabftw/CustomMigrationRunner.php
+++ src/Elabftw/CustomMigrationRunner.php
@@ -76,6 +76,8 @@ final class CustomMigrationRunner
         '036_notif_task_assigned.sql',
         '037_todolist_columns_per_project.sql',
         '038_project_details_and_pinning.sql',
+        '050_todolist_project_archive.sql',
+        '051_todolist_project_ordering.sql',
     );
 
     private Db $Db;
